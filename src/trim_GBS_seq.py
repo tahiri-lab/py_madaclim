6bfaa4b2992616ee3ccfe11c14a1aa425d8bb74b
simlal <simlalonde@hotmail.com>
2023-01-23 16:49:54 -0500

    Trim .fasta and write to file

---
+++ src/trim_GBS_seq.py
@@ -0,0 +1,26 @@
+from definitions import config,SNP_DIR, SNP_TRIM_DIR  # Get global vars
+from pathlib import Path
+
+# Number of bases to keep
+seq_len = config["genetic"]["bp_to_keep"]
+
+# Get all sequencing file names in data/GBS dir
+gbs_filenames = [file.name for file in SNP_DIR.iterdir() if SNP_DIR.is_dir() and file.is_file()]
+
+if __name__ == "__main__":
+    # Looping through full SNP fasta files
+    for full_fasta in SNP_DIR.iterdir():
+        if full_fasta.is_file():
+            with open(full_fasta, "r") as fasta_in:
+                id = fasta_in.readline().strip("\n") # Catch the sequence id
+                seq_trimmed = ("".join([line.rstrip() for line in fasta_in])[:seq_len+1])  # Get first bp up to seq_len
+                # Save to new dir/file with _trimmed tag
+                with open(SNP_TRIM_DIR / (full_fasta.stem + "_trimmed" + full_fasta.suffix), "w") as out_file:
+                    out_file.write(f"{id}")
+                    # Newline every 80th character
+                    for i, nucleotide in enumerate(seq_trimmed):
+                        if i % 80 == 0 :
+                            out_file.write(f"\n")
+                        else :
+                            out_file.write(nucleotide)
+            
