4118f12eb4cc36a6d7662e599a351dd2ea89dd26
simlal <simlalonde@hotmail.com>
2023-01-23 14:37:35 -0500

    Add data, module, yaml-vars to globals

--- definitions.py
+++ definitions.py
@@ -1,11 +1,18 @@
+# Get cross-system compatible paths using pathlib
 from pathlib import Path
 
-# Get user-specific directory path for the main project/root folder
+# Main project/root folder
 ROOT_DIR = Path(__file__).parent
+
+# Import YAML config variables + params
+import yaml
+with open(ROOT_DIR / "config.yaml", "r") as yf:
+    config = yaml.safe_load(yf)
 
 # Data-relevant paths
 SNP_DIR = ROOT_DIR.joinpath("data", "GBS")
 GPS_DIR = ROOT_DIR.joinpath("data", "geospatial")
+SNP_TRIM_DIR = ROOT_DIR.joinpath("data", config["trimmed_dirs"]["GBS_in"])
 
 # Module paths
 GENETIC_DIR = ROOT_DIR.joinpath("src", "genetic")
