947d7ddd189255c73340a995049dbeab9bd373a1
simlal <simlalonde@hotmail.com>
2023-01-23 14:20:11 -0500

    Add data and module paths

--- definitions.py
+++ definitions.py
@@ -3,3 +3,10 @@ from pathlib import Path
 # Get user-specific directory path for the main project/root folder
 ROOT_DIR = Path(__file__).parent
 
+# Data-relevant paths
+SNP_DIR = ROOT_DIR.joinpath("data", "GBS")
+GPS_DIR = ROOT_DIR.joinpath("data", "geospatial")
+
+# Module paths
+GENETIC_DIR = ROOT_DIR.joinpath("src", "genetic")
+CLIMATE_DIR = ROOT_DIR.joinpath("src", "climate")
