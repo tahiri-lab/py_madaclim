c40bb59bbac83143e30a1715d36ff403034ab14d
simlal <simlalonde@hotmail.com>
2023-01-24 17:32:24 -0500

    Update with new yaml refs

--- src/definitions.py
+++ src/definitions.py
@@ -1,7 +1,7 @@
 # Get cross-system compatible paths using pathlib
 from pathlib import Path
 
-# Main project/root folder
+# Relative main folder path
 ROOT_DIR = Path(__file__).parents[1]
 
 # Import YAML config variables + params
@@ -9,11 +9,21 @@ import yaml
 with open(ROOT_DIR / "config.yaml", "r") as yf:
     config = yaml.safe_load(yf)
 
-# Data-relevant paths
-SNP_DIR = ROOT_DIR.joinpath("data", "gbs")
-GPS_DIR = ROOT_DIR.joinpath("data", "geospatial")
-SNP_TRIM_DIR = ROOT_DIR.joinpath("data", config["trimmed_dirs"]["gbs_in"])
+# Main data dir path
+DATA_DIR = ROOT_DIR / "data"
 
-# Module paths
-GENETIC_DIR = ROOT_DIR.joinpath("src", "genetic")
-CLIMATE_DIR = ROOT_DIR.joinpath("src", "climate")
+# Genetic data dirs
+GENETIC_DIR = DATA_DIR / config["genetic"]["dir"]["main"]
+SNP_DIR = GENETIC_DIR / config["genetic"]["dir"]["gbs_full"]
+SNP_TRIM_DIR = GENETIC_DIR / config["genetic"]["dir"]["gbs_trimmed"]
+
+# Geoclimatic data dirs
+GEOCLIM_DIR = DATA_DIR / config["geoclim"]["dir"]["main"]
+GEOSPATIAL_DIR = GEOCLIM_DIR / config["geoclim"]["dir"]["gps_data"]
+CLIMATE_DIR = GEOCLIM_DIR / config["geoclim"]["dir"]["climate_data"]
+
+# Modules path
+GENETIC_MOD = ROOT_DIR.joinpath("src", "genetic")
+GEOCLIM_MOD = ROOT_DIR.joinpath("src", "climate")
+
+print(SNP_DIR)
