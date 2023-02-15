f9f1cddd159015442b8d2e6f8d1b979cbf33fc93
simlal <simlalonde@hotmail.com>
2023-02-15 15:12:12 -0500

    Initial unit tests madaclim class

---
+++ src/tests/test_madaclim_layers.py
@@ -0,0 +1,9 @@
+import unittest
+from geospatial.madaclim import MadaClimLayers
+
+class TestMadaclimLayers(unittest.TestCase):
+    
+    def test_get_clim_layers(self):
+        madalayers = MadaClimLayers()
+        other = "test"
+        self.assertEqual()
