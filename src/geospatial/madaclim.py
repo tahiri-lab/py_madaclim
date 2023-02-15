ba890417aad5589edd0af371252de77128653c40
simlal <simlalonde@hotmail.com>
2023-02-15 15:13:05 -0500

    Initial commit madaclimlayers class

---
+++ src/geospatial/madaclim.py
@@ -0,0 +1,12 @@
+
+class MadaClimLayers:
+    def __init__(self) -> None:
+        pass
+    
+    def get_clim_layers(self):
+        x = "test"
+        return x
+
+if __name__ == "__main__":
+    test = MadaClimLayers()
+    print(test.get_clim_layers())
