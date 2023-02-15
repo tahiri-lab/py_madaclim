a486306947bf35544674236bef27b08264dc7055
simlal <simlalonde@hotmail.com>
2023-02-15 15:54:45 -0500

    Test package import in -e mode

--- src/tests/test_madaclim_layers.py
+++ src/tests/test_madaclim_layers.py
@@ -6,4 +6,8 @@ class TestMadaclimLayers(unittest.TestCase):
     def test_get_clim_layers(self):
         madalayers = MadaClimLayers()
         other = "test"
-        self.assertEqual()
+        self.assertEqual(madalayers.get_clim_layers(), other)
+
+if __name__ == "__main__":
+    mada_test = MadaClimLayers()
+    print(mada_test.get_clim_layers())
