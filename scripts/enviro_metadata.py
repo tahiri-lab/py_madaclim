4d1333fb0f546f118074ea493867bc9794083001
simlal <simlalonde@hotmail.com>
2023-03-30 12:39:09 -0400

    Fix dir paths

--- scripts/enviro_metadata.py
+++ scripts/enviro_metadata.py
@@ -1,7 +1,16 @@
+import yaml
 import pandas as pd
 from pathlib import Path
 
-root_dir = Path(__file__).parent
+ROOT_DIR = Path(__file__).parents[1]
+SRC_DIR = ROOT_DIR / "src"
+
+with open(SRC_DIR.joinpath("config.yaml"), "r") as yaml_file:
+    config = yaml.safe_load(yaml_file)
+
+# Target dir
+ENVIRO_DIR = ROOT_DIR / "data" / config["geoclim"]["dir"]["main"] / config["geoclim"]["dir"]["environment_data"]
+
+# Get Environmental metadata and save
 enviro_meta = pd.read_html("https://madaclim.cirad.fr/environ-data-format/", index_col="Layer")[0]
-# print(enviro_meta)
-enviro_meta.to_csv(root_dir / "enviro_metadata.csv")
+enviro_meta.to_csv(ENVIRO_DIR / "enviro_metadata.csv")
