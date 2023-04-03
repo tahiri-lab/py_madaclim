605b9fdf9f9f22db67fa37907b8f3e7ad45e6e8d
simlal <simlalonde@hotmail.com>
2023-04-03 14:59:49 -0400

    Test cfg filename ref attribute change

--- src/coffeaphylogeo/tests/test_definitions.py
+++ src/coffeaphylogeo/tests/test_definitions.py
@@ -3,10 +3,26 @@ from coffeaphylogeo.definitions import Definitions
 
 class TestDefinitions(unittest.TestCase):
     
-    def test_config_attribute(self):
+    def test_configfilename_attribute_name(self):
         defs = Definitions()
-        defs.config_filename = "test_config.yaml"
-        self.assertEqual(defs.config_filename, "test_config.yaml")
+        print(defs.config_filename)
+        defs.config_filename = "bioclim_chelsa.yaml"
+        print(f"After change : {defs.config_filename}")
+
+        self.assertEqual(defs.config_filename, "bioclim_chelsa.yaml")
+
+    def test_config_reference_with_attribute_change(self):
+        defs = Definitions()
+        init_keys = defs.config.keys()
+        print(init_keys)
+        
+        # Change the config_filename and test if refs change
+        defs.config_filename = "bioclim_chelsa.yaml"
+        refactor_keys = defs.config.keys()
+        print(refactor_keys)
+
+        self.assertNotEqual(init_keys, refactor_keys)
+
 
 if __name__ == "__main__":
     unittest.main()
